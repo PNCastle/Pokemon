7e3d6c23c505ccd5a95c6f29af05e44754d0ea0f
Castleberry <pncastle@oscr-mz14.catnet.arizona.edu>
2017-04-16 14:06:29 -0700

    updates to map and trainer

--- src/Model/Map.java
+++ src/Model/Map.java
@@ -1,5 +1,7 @@
 package Model;
 
+import java.awt.Color;
+import java.awt.Graphics2D;
 import java.io.BufferedReader;
 import java.io.FileReader;
 
@@ -17,10 +19,12 @@ public class Map {
 	private int tileSize;
 	private int x;
 	private int y;
-	private char[][] currentMap;
+	private int[][] currentMap;
+	private int mapWidth; 	//tile#
+	private int mapHeight; 	//tile#
 	
 	//ctor
-	public Map(String fileName, int tielSize) {
+	public Map(String fileName, int tileSize) {
 		//currentMap = new char[30][22];
 		//if statement for which  map
 		this.tileSize = tileSize;
@@ -29,12 +33,64 @@ public class Map {
 			
 			BufferedReader br = new BufferedReader(new FileReader(fileName));
 		
+			mapWidth = Integer.parseInt(br.readLine());
+			mapHeight = Integer.parseInt(br.readLine());
+			
+			currentMap = new int[mapHeight][mapWidth];
+			
+			String delimiters = " ";
+			
+			for(int row = 0; row < mapHeight; row++) {
+				String line = br.readLine();
+				String[] tokens = line.split(delimiters);
+				for (int col = 0; col < mapWidth; col++) {
+					currentMap[row][col] = Integer.parseInt(tokens[col]);
+				}
+				
+				
+			}
+			
+			
 		}
 		
 		catch(Exception e) {	
 		}
 		
-		currentMap = buildMapOne();
+		//currentMap = buildMapOne();
+	}
+	
+	public void update() {
+		
+	}
+	
+	// 0 = blocked     = black
+	// 1 = trail       = white
+	// 2 = short grass = light green
+	// 3 = tall grass  = dark green
+	public void draw(Graphics2D g) {
+		
+		for(int row = 0; row < mapHeight; row++) {
+			for(int col = 0; col < mapWidth; col++) {
+				
+				int rc = currentMap[row][col];
+				
+				if(rc == 0) {
+					g.setColor(Color.BLACK);
+				}
+				if(rc == 1) {
+					g.setColor(Color.WHITE);
+				}
+				if(rc == 2) {
+					g.setColor(Color.gray);
+				}
+				if(rc == 3) {
+					g.setColor(Color.GREEN);
+				}
+				g.fillRect(x + col * tileSize,  y + row * tileSize,  tileSize,  tileSize);
+				
+			}
+		}
+		
 	}
 	
 	//to string method
@@ -89,6 +145,14 @@ public class Map {
 			}
 		}
 		return tempMap;
+	}
+
+	public int getX() {
+		return this.x;
+	}
+
+	public int getY() {
+		return this.y;
 	}
 
 }
--- src/Model/Trainer.java
+++ src/Model/Trainer.java
@@ -1,5 +1,69 @@
 package Model;
 
+import java.awt.Color;
+import java.awt.Graphics2D;
+
 public class Trainer {
 
+	private double x;
+	private double y;
+	private double dx;
+	private double dy;
+	
+	private int width;
+	private int height;
+	
+	private boolean left;
+	private boolean right;
+	private boolean up;
+	private boolean down;
+	
+	private double moveSpeed;
+	private double maxSpeed;
+//	private double stopSpeed;
+	
+	private Map map;
+	
+	public Trainer(Map map) {
+		
+		this.map = map;
+		
+		this.width = 20;
+		this.height = 20;
+		
+		moveSpeed = .5;
+		maxSpeed = 4;
+		
+	}
+	
+	public void setLeft(boolean b) {
+		this.left = b;
+	}
+	public void setRight(boolean b) {
+		this.right = b;
+	}
+	public void setUp(boolean b) {
+		this.up = b;
+	}
+	public void setDown(boolean b) {
+		this.down = b;
+	}
+	
+	public void update() {
+		
+	}
+	
+	public void draw(Graphics2D g) {
+		int tileX = map.getX();
+		int tileY = map.getY();
+		
+		g.setColor(Color.RED);
+		g.fillRect(
+				(int) (tileX + x - width/2),
+				(int) (tileY + y - height/2),
+				width,
+				height
+		);
+	}
+	
 }
