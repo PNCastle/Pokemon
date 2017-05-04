ee16cc86bbb4b8f0b256c743981b71c14344ce7e
PNCastle <pncastle@email.arizona.edu>
2017-05-03 18:38:46 -0700

    new images

--- src/View/MapPanel.java
+++ src/View/MapPanel.java
@@ -41,7 +41,7 @@ public class MapPanel extends JPanel implements Runnable {
 	private BufferedImage image;
 	private Graphics2D g;
 
-	private int FPS = 50;
+	private int FPS = 15;
 	private int targetTime = 1000 / FPS;
 
 	int red = 255;
